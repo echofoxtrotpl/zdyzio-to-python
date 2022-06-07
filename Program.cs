f5fed43b2485d1b877a2af372634685e8d8ea2ce
Jakub <33374952+kuballa@users.noreply.github.com>
2022-06-07 19:33:08 +0200

    Dodanie 5 plików testowych

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
-﻿using Antlr4.Runtime;
+using Antlr4.Runtime;
 using ZdyzioToPython;
 using ZdyzioToPython.Content;
 
-var fileName = @"Inputs/test1.txt";
+//var fileName = @"Inputs/test1.txt";
 
-var fileContents = File.ReadAllText(fileName);
+//var fileContents = File.ReadAllText(fileName);
 
-var inputStream = new AntlrInputStream(fileContents);
-var zdyzioLexer = new ZdyzioLexer(inputStream);
-var commonTokenStream = new CommonTokenStream(zdyzioLexer);
-var zdyzioParser = new ZdyzioParser(commonTokenStream);
-var zdyzioContext = zdyzioParser.program();
-var visitor = new ZdyzioVisitor();
+//var inputStream = new AntlrInputStream(fileContents);
+//var zdyzioLexer = new ZdyzioLexer(inputStream);
+//var commonTokenStream = new CommonTokenStream(zdyzioLexer);
+//var zdyzioParser = new ZdyzioParser(commonTokenStream);
+//var zdyzioContext = zdyzioParser.program();
+//var visitor = new ZdyzioVisitor();
 
-var result = visitor.Visit(zdyzioContext);
+//var result = visitor.Visit(zdyzioContext);
 
-File.WriteAllText(@"result.py",result);
+//File.WriteAllText(@"result.py",result);
 
-Console.WriteLine(result);
+//Console.WriteLine(result);
+
+for (int i = 1; i < 6; i++)
+{
+    var fileName = $@"Inputs/test{i}.txt";
+    var fileContents = File.ReadAllText(fileName);
+
+    var inputStream = new AntlrInputStream(fileContents);
+    var zdyzioLexer = new ZdyzioLexer(inputStream);
+    var commonTokenStream = new CommonTokenStream(zdyzioLexer);
+    var zdyzioParser = new ZdyzioParser(commonTokenStream);
+    var zdyzioContext = zdyzioParser.program();
+    var visitor = new ZdyzioVisitor();
+
+    var result = visitor.Visit(zdyzioContext);
+
+    File.WriteAllText(@$"result{i}.py", result);
+    Console.WriteLine($"Result {i}\n");
+    Console.WriteLine(result);
+    Console.WriteLine();
+}
