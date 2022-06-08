83613216f0a1c298ecbe457ac67af2caaa13efff
Jakub <33374952+kuballa@users.noreply.github.com>
2022-06-08 15:39:28 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,23 +2,6 @@ using Antlr4.Runtime;
 using ZdyzioToPython;
 using ZdyzioToPython.Content;
 
-//var fileName = @"Inputs/test1.txt";
-
-//var fileContents = File.ReadAllText(fileName);
-
-//var inputStream = new AntlrInputStream(fileContents);
-//var zdyzioLexer = new ZdyzioLexer(inputStream);
-//var commonTokenStream = new CommonTokenStream(zdyzioLexer);
-//var zdyzioParser = new ZdyzioParser(commonTokenStream);
-//var zdyzioContext = zdyzioParser.program();
-//var visitor = new ZdyzioVisitor();
-
-//var result = visitor.Visit(zdyzioContext);
-
-//File.WriteAllText(@"result.py",result);
-
-//Console.WriteLine(result);
-
 for (int i = 1; i < 6; i++)
 {
     var fileName = $@"Inputs/test{i}.txt";
